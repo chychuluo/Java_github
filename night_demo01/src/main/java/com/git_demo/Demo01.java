6168b7b1346c1c1d7332b7d0bacc168b4d0eb9ac
chy <chy@example.com>
2023-08-14 22:07:53 +0800

    测试22

--- night_demo01/src/main/java/com/git_demo/Demo01.java
+++ night_demo01/src/main/java/com/git_demo/Demo01.java
@@ -5,5 +5,6 @@ public class Demo01 {
     public static void main(String[] args) {
         System.out.println("github,hello");
         System.out.println("github,hello");
+        System.out.println(true);
     }
 }
